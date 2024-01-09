4aea3a3b7167411128423037f3f3384420381570
林北 <67180012+wdnmdhhhh@users.noreply.github.com>
2024-01-09 15:09:42 +0800

    Update weather_report.py

--- weather_report.py
+++ weather_report.py
@@ -11,8 +11,6 @@ appSecret = os.environ.get("APP_SECRET")
 openId = os.environ.get("OPEN_ID")
 # 天气预报模板ID
 weather_template_id = os.environ.get("TEMPLATE_ID")
-# 时间表模板ID
-timetable_template_id = "CCCWC3o9Mlc3hRvlODVFze7WFvUDww95-7bWLsk6BW0"
 
 def get_weather(my_city):
     urls = ["http://www.weather.com.cn/textFC/hb.shtml",
@@ -132,4 +130,4 @@ def weather_report(this_city):
 
 
 if __name__ == '__main__':
-    weather_report("淄博")
+    weather_report("重庆")
